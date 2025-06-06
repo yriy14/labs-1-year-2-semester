5cfa588a9d1342f609d3bb40fd47b24196b07ebf
Yurii <fliakyrez@gmail.com>
2025-06-06 09:16:16 +0300

    Add files via upload

---
+++ src/lab_7_code.py
@@ -0,0 +1,48 @@
+def naive_search_last(haystack, needle):
+    hay_len = 0
+    for _ in haystack:
+        hay_len += 1
+
+    needle_len = 0
+    for _ in needle:
+        needle_len += 1
+
+    last_index = -1
+    comparisons = 0
+
+    for i in range(hay_len - needle_len + 1):
+        match = True
+        for j in range(needle_len):
+            comparisons += 1
+            if haystack[i + j] != needle[j]:
+                match = False
+                break
+        if match:
+            last_index = i
+
+    return last_index, comparisons
+
+def read_input(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = []
+        for line in f:
+            lines.append(line.strip())
+    haystack = ""
+    needle = ""
+
+    for line in lines:
+        if line.startswith("haystack:"):
+            haystack = line[len("haystack:"):].strip()
+        elif line.startswith("needle:"):
+            needle = line[len("needle:"):].strip()
+
+    return haystack, needle
+
+def write_output(filename, index, comparisons):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write("Останній індекс входження: " + str(index) + "\n")
+        f.write("Кількість порівнянь: " + str(comparisons) + "\n")
+
+haystack, needle = read_input("C:/Users/User/Desktop/Labs/2 semester/lab_7/input.txt")
+index, comparisons = naive_search_last(haystack, needle)
+write_output("C:/Users/User/Desktop/Labs/2 semester/lab_7/output.txt", index, comparisons)
