64eb2bc163438b360b50ab7ffa6d3aa8b0200bda
Yurii <fliakyrez@gmail.com>
2025-06-06 09:11:00 +0300

    lab_8_test.py

--- test/lab_8_test.py
+++ test/lab_8_test.py
@@ -1,10 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 import unittest
 from lab_8_main import read_graph_from_txt, max_flow
 
 
 class TestFlowerDelivery(unittest.TestCase):
     def test_manual_graph(self):
-        farms, shops, capacity = read_graph_from_txt("C:/Users/User/Desktop/Labs/2 semester/lab_8/roads.txt")
+        farms, shops, capacity = read_graph_from_txt("C:/Users/User/Desktop/work/roads.txt")
         result = max_flow(farms, shops, capacity)
         self.assertIsInstance(result, int)
         self.assertEqual(result, 8)
