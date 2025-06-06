ab8cafd9b9f413891ce362d1fe1f88fbd2aa5721
Yurii <fliakyrez@gmail.com>
2025-06-06 09:39:05 +0300

    Add files via upload

---
+++ test/lab_4_test.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import unittest
+from lab_4_code import PriorityQueue
+
+
+class TestPriorityQueue(unittest.TestCase):
+    def test_queue_operations(self):
+        pq = PriorityQueue()
+        pq.insert("A", 2)
+        pq.insert("B", 5)
+        pq.insert("C", 1)
+
+        self.assertEqual(pq.view(), [("C", 1), ("A", 2), ("B", 5)])
+        self.assertEqual(pq.pop(), ("B", 5))
+        self.assertEqual(pq.pop(), ("A", 2))
+        self.assertEqual(pq.pop(), ("C", 1))
+        self.assertIsNone(pq.pop())
+
+
+if __name__ == "__main__":
+    unittest.main()
